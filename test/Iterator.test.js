41796c56a5afa5902c57d4b15fb2a4b9d7d5ca7d
alexmainbrain <alexmainbrain7@gmail.com>
2019-05-22 10:27:29 +0300

    hw12

---
+++ test/Iterator.test.js
@@ -0,0 +1,44 @@
+const expect = require("chai").expect;
+const Iterator = require(`../test/Iterator`)
+
+describe('test Iterator module', () => {
+    let value = 0;
+    const iterator = new Iterator(value);
+    it('get current === 0', () => {
+        const current = iterator.current();
+        expect(current).to.be.equal(0)
+    });
+    it('return next() -> get current === 1', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(1)
+    });
+    it('return next() -> get current === 1', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(1)
+    })
+    it('return next() -> get current === 2', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(2)
+    })
+    it('return next() -> get current === 3', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(3)
+    })
+    it('return next() -> get current === 5', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(5)
+    })
+    it('return next() -> get current === 8', () => {
+        let next = iterator.next();
+        expect(next).to.be.equal(8)
+    })
+    it('return key() -> get current === 6 ', () => {
+        let key = iterator.key();
+        expect(key).to.be.equal(6)
+    })
+    it('return rewind()  -> get current === 0 ', () => {
+        let rewind = iterator.rewind();
+        expect(rewind).to.be.equal(0)
+    })
+    
+})
